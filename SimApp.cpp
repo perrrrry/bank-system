f500debfa5f40617f1161d3f6c9f9d26dc6c5154
perrrrry <48076249+perrrrry@users.noreply.github.com>
2019-02-27 19:03:05 -0800

    Update SimApp.cpp

--- SimApp.cpp
+++ SimApp.cpp
@@ -4,17 +4,35 @@
 #include "PriorityQueue.h"
 #include "EmptyDataCollectionException.h"
 
+#include <fstream>
+#include <string>
+
 #include <iostream>
 
 using namespace std;
 
+#define Teller_number 1;  // change to different number in order to compare their waiting time, and to decide if hire more tellers
+
 int main () {
 
+string STRING;	//create a string to kepp tracking the file from input file
+ifstream infile;
+infile.open ("simulationShuffled1.in"); // read the file based on name 
+	while(!infile.eof) // To get all the lines.
+	{
+		getline(infile,STRING); // Saves the line in STRING.
+		cout<<STRING; // Prints our STRING.
+	}
+infile.close();
+system ("pause");
 
 
 
 
-
+	
+	
+	
+	
 
 
 	
